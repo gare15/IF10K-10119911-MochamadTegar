fcf70eaa9648ff3107b297413e7a6484a0a57fc1
gare15 <72866876+gare15@users.noreply.github.com>
2020-10-21 07:58:18 +0700

    Create IF10K10119911Latihan1Biodata.java

---
+++ src/PBO/pkg10119911/biodata/IF10K10119911Latihan1Biodata.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119911.latihan1.biodata;
+
+/**
+ *
+ * @author gare15
+ * Nama      : Mochamad Tegar Dwi S
+ * NIM       : 10119911
+ * Kelas     : IF-10K
+ * Deskripsi : Perintah menampilkan value ke layar.
+ */
+public class IF10K10119911Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------");
+        System.out.println("Nama\t\t: Mochamad Tegar Dwi S");
+        System.out.println("Alamat\t\t: Kp. Gunung Masigit Rt01/08");
+        System.out.println("Tempat Lahir\t: Bandung");
+        System.out.println("Tanggal Lahir\t: 15 September 2001");
+        System.out.println("----------------");
+        System.out.println("Hobi\t\t: Renang");
+        System.out.println("Makes\t\t: Ayam");
+        System.out.println("Mikes\t\t: Susu");
+    }
+    
+}
